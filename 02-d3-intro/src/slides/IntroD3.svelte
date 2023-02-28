5cc555a8b30258f1a9955ee6d4e9a9c92200ba53
romanoe <noemi.romano@protonmail.com>
2023-02-28 20:53:30 +0100

    joining data with animation

--- 02-d3-intro/src/slides/IntroD3.svelte
+++ 02-d3-intro/src/slides/IntroD3.svelte
@@ -2,13 +2,17 @@
     import Slide from '../lib/Slide.svelte';
     import Code from "../lib/Code.svelte";
     import {select, selectAll} from 'd3-selection';
+    import {shuffle} from "d3-array";
     import {onMount} from "svelte";
+    import {transition} from "d3-transition";
+    import {easeLinear} from "d3-ease";
     import Fa from 'svelte-fa'
     import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
     const width = 500
     const height = 300
+    const margin = {top : 50, right: 40, bottom: 50, left: 40};
 
     let x = 0;
     let y = 0;
@@ -50,6 +54,112 @@
             .data(data)
             .join(enter => enter.append("li").text((d, i) => 'Valeur: ' + d + ', Index: ' + i))
             .style("color", 'green');
+
+
+
+
+
+        const svgJoin = select('#viz_area')
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            // translate this svg element to leave some margin.
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        const svgJoin2 = select('#viz_area2')
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            // translate this svg element to leave some margin.
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+        const svgJoin3 = select('#viz_area3')
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            // translate this svg element to leave some margin.
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+        // Enter
+        const tableauLettresAleatoires = () => {
+            return shuffle("abcdefghijklmnopqrstuvwxyz".split(""))
+                .slice(0, Math.floor(6 + Math.random() * 20))
+                .sort();
+        }
+
+
+
+        setInterval(() => {
+            svgJoin.selectAll("text")
+                .data(tableauLettresAleatoires(), d => d)
+                .join(
+                    enter => enter.append("text")
+                        .attr('fill', 'green')
+                        .text(d => d)
+
+                )
+                .attr("x", (d, i) => i * 30) // Mettre à jour à chaque fois la position
+
+        }, 1000);
+
+
+
+
+
+        setInterval(() => {
+            svgJoin2.selectAll("text")
+                .data(tableauLettresAleatoires(), d=>d)
+                .join(
+                    enter => enter.append("text")
+                        .attr('fill', 'green')
+                        .attr("x", (d, i) => i * 30) // Mettre à jour à chaque fois la position
+                        .text(d => d),
+                    update => update.attr('fill', 'grey')
+                        .attr("x", (d, i) => i * 30) // Mettre à jour à chaque fois la position
+                )
+
+        }, 1000);
+
+
+
+        function getTransition() {
+            return transition()
+                .duration(500)
+                .ease(easeLinear)
+        }
+
+        setInterval(() => {
+            svgJoin3.selectAll("text")
+                .data(tableauLettresAleatoires, d => d)
+                .join(
+                    enter => enter.append("text")
+                        .attr("fill", "green")
+                        .attr("x", (d, i) => i * 30)
+                        .attr("y", 0)
+                        .text(d => d)
+                     ,
+                    update => update
+                        .attr("fill", "grey")
+                        .attr("y", 0)
+                        .attr("x", (d, i) => i * 30),
+                    exit => exit
+                        .attr("fill", "brown")
+                        .transition(getTransition())
+                        .attr("y", 30)
+                        .remove()
+                );
+        }, 2000);
+
+
+
+
+
     });
 
 </script>
@@ -68,17 +178,26 @@
 
 <Slide>
     <h2>Documentation</h2>
+    <br>
     <a href="https://github.com/d3/d3/blob/main/API.md"><code>Github: API reference</code></a>
+
+    <br>
+    <br>
+    <img src="assets/doc_annotation.svg"  alt="js-doc" class="center"> <br>
+
+
+
+
 </Slide>
 
-<Slide>
-    <h2>DOM</h2>
-    <p><span class="heig-red">D</span>ocument <span class="heig-red">O</span>bject <span class="heig-red">M</span>odel</p>
-    <small><blockquote>
-        Le Document Object Model (DOM) est une interface de programmation normalisée par le W3C, qui permet à des scripts d'examiner et de modifier le contenu du navigateur web
-    </blockquote></small>
-    <p class="fragment">(en bref : structure hiérarchique de <code>HTML</code>)</p>
-</Slide>
+<!--<Slide>-->
+<!--    <h2>DOM</h2>-->
+<!--    <p><span class="heig-red">D</span>ocument <span class="heig-red">O</span>bject <span class="heig-red">M</span>odel</p>-->
+<!--    <small><blockquote>-->
+<!--        Le Document Object Model (DOM) est une interface de programmation normalisée par le W3C, qui permet à des scripts d'examiner et de modifier le contenu du navigateur web-->
+<!--    </blockquote></small>-->
+<!--    <p class="fragment">(en bref : structure hiérarchique de <code>HTML</code>)</p>-->
+<!--</Slide>-->
 
 
 <Slide>
@@ -107,8 +226,8 @@
 <Slide>
     <h2>Sélectionner</h2>
 
-    <code>.select(<em>sélecteur</em>)</code> <br>
-    <code>.selectAll(<em>sélecteur</em>)</code>
+    <code><b class="heig-red">select</b>(<em>selector</em>)</code> <br>
+    <code><b class="heig-red">selectAll</b>(<em>selector</em>)</code>
 
     <br>
     <br>
@@ -141,7 +260,7 @@
     <br>
     <br>
     <br>
-    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection</code></p></a></small>
+    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0#selecting-elements"><p> <code class="heig-red"> ↳ d3-selection - Selecting elements</code></p></a></small>
 
 
 </Slide>
@@ -152,7 +271,7 @@
     <h2>Modifier</h2>
 
     <br>
-    <code>attr('nom_attribut','valeur')</code>
+    <code>selection.<b class="heig-red">attr</b>(name[, value])</code>
     <div  class="row no-margin-top">
         <div class="col-60">
 
@@ -188,7 +307,7 @@ cercle.attr("fill", "#E92528");
 
         </div>
     </div>
-    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection</code></p></a></small>
+    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0#modifying-elements"><p> <code class="heig-red"> ↳ d3-selection - Modifying elements</code></p></a></small>
 
 </section>
 
@@ -197,7 +316,7 @@ cercle.attr("fill", "#E92528");
 <Slide>
     <h2>Ajouter</h2>
     <br>
-    <code>.append("element-html")</code>
+    <code>selection.<b class="heig-red">append</b>(type)</code>
     <div class="row no-margin-top">
         <div class="col-60">
 
@@ -223,7 +342,7 @@ const div = select("#mon-svg")
         </svg>
         </div>
     </div>
-    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection</code></p></a></small>
+    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection -  Modifying elements</code></p></a></small>
 
 </Slide>
 
@@ -231,7 +350,7 @@ const div = select("#mon-svg")
 <Slide>
     <h2>Ecouter les événements</h2>
 
-    <code>.on("événmentent", fonctions)</code>
+    <code>selection.<b class="heig-red">on</b>(typenames[, listener[, options]])</code>
     <div class="row no-margin-top">
         <div class="col-60">
 				<pre><Code trim>
@@ -270,6 +389,7 @@ select("#mon-svg")
 
         <svg id="mon-svg3">
             <circle cx="50%" cy="50%" r="100"></circle>
+            <circle cx="50%" cy="50%" r="100"></circle>
         </svg>
 
 
@@ -279,24 +399,106 @@ select("#mon-svg")
 
 
     </div>
-    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection</code></p></a></small>
+    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0#handling-events"><p> <code class="heig-red"> ↳ d3-selection - Handling events</code></p></a></small>
 
 </Slide>
+
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <p class="heig-red"><em>Joindre les données aux éléments <b>sélectionnés</b></em></p>
+    <br>
+
+    <code>
+        selection.<b class="heig-red">data</b>([data[, key]])
+    </code>
+
+    <br>
+    <br>
+    <br>
+
+    <code>
+        selection.<b class="heig-red">data</b>(<em>tableau</em>).<b class="heig-red">join</b>(enter[, update][, exit])
+    </code>
+
+    <br>
+
+</Slide>
+
+
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <h5><span class="heig-red">Entering data</span></h5>
+
+    <div class="row">
+        <img src="assets/data-enter-2.svg" alt="" height="300"/>
+    </div>
+</Slide>
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <h5><span class="heig-red">Mise à jour des données</span></h5>
+    <code>enter, update, exit</code>
+    <div class="row">
+        <img src="assets/data-enter-update-exit.svg" alt="" height="300"/>
+    </div>
+</Slide>
+
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <h5><span class="heig-red">EXEMPLE</span></h5>
+    <div class="row">
+        <div class="col-50">
+            <img src="assets/data-join-example-1.svg" alt="" height="300"/>
+        </div>
+        <div class="col-50">
+
+            <div id="viz_area"></div>
+
+        </div>
+    </div>
+</Slide>
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <h5><span class="heig-red">EXEMPLE</span></h5>
+    <div class="row">
+        <div class="col-50">
+            <img src="assets/data-join-example-2.svg" alt="" height="300"/>
+        </div>
+        <div class="col-50">
+            <div id="viz_area2"></div>
+
+        </div>
+    </div>
+</Slide>
+
+
+
+<Slide>
+    <h2>Joindre les données</h2>
+    <h5><span class="heig-red">EXEMPLE</span></h5>
+    <div class="row">
+        <div class="col-50">
+            <img src="assets/data-join-example-3.svg" alt="" height="300"/>
+        </div>
+        <div class="col-50">
+            <div id="viz_area3"></div>
+        </div>
+    </div>
+</Slide>
+
+
+
 
 <Slide>
 
     <h2>Joindre les données</h2>
-    <p class="heig-red"><em>Joindre les données aux éléments <b>sélectionnés</b></em></p>
 
 
-    <code>
-        selection.data([data[, key]])
-    </code>
 
-    <code>
-        selection.join(enter[, update][, exit])
-    </code>
-    <br>
     <div class="row">
 
 
@@ -354,7 +556,7 @@ select("#ma-div")
 
 
 
-    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0"><p> <code class="heig-red"> ↳ d3-selection</code></p></a></small>
+    <small><a href="https://github.com/d3/d3-selection/tree/v3.0.0#joining-data"><p> <code class="heig-red"> ↳ d3-selection - Joining data</code></p></a></small>
 
 </Slide>
 
