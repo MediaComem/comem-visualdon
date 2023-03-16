d5089baf517b30d7e559b341a67f14b77a38133f
romanoe <noemi.romano@protonmail.com>
2023-03-16 21:00:14 +0100

    cours 6 + cours 7

---
+++ 06-interaction-animation-2/src/Presentation.svelte
@@ -0,0 +1,8 @@
+<script>
+import Title from "./slides/Title.svelte";
+import InteractionAnimation2 from "./slides/InteractionAnimation2.svelte";
+</script>
+
+
+<Title title="6. Interaction et animation 2"></Title>
+<InteractionAnimation2></InteractionAnimation2>
