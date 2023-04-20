eb27a5782ce22a81e3368ee2571da796ea7c1493
romanoe <noemi.romano@protonmail.com>
2023-04-20 21:19:15 +0200

    do and donts

---
+++ 10-do-and-donts/src/Presentation.svelte
@@ -0,0 +1,8 @@
+<script>
+import Title from "./slides/Title.svelte";
+import DoAndDonts from "./slides/DoAndDonts.svelte";
+</script>
+
+
+<Title title="10. Do and Don'ts en visualisation de données"></Title>
+<DoAndDonts></DoAndDonts>
