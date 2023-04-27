a7345f25b9c05ab415f8364ca88990068314ed0b
romanoe <noemi.romano@protonmail.com>
2023-04-27 22:16:16 +0200

    cours 11

---
+++ 11-d3-alternatives-scrollytelling/src/Presentation.svelte
@@ -0,0 +1,8 @@
+<script>
+import Title from "./slides/Title.svelte";
+import D3AlternativesScrollyTelling from "./slides/D3AlternativesScrollyTelling.svelte"
+</script>
+
+
+<Title title="11. Alternatives d3 + Scrollytelling"></Title>
+<D3AlternativesScrollyTelling></D3AlternativesScrollyTelling>
