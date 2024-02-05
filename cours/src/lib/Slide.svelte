b3c600fbaf36f147c4b84994f199ecd8aa81a454
romanoe <noemi.romano@protonmail.com>
2024-02-05 21:10:36 +0100

    fix: remove background

--- cours/src/lib/Slide.svelte
+++ cours/src/lib/Slide.svelte
@@ -1,22 +1,22 @@
 <script>
   // Color Backgrounds
-  export let bgColor = "#fff";
+  export let bgColor;
 
   // Gradient Backgrounds
-  export let bgGradient = "#fff";
+  export let bgGradient;
   // Image Backgrounds
-  export let bgImage = "";
-  export let bgSize = "";
-  export let bgPosition = "";
-  export let bgRepeat = "";
-  export let bgOpacity = "";
+  export let bgImage;
+  export let bgSize;
+  export let bgPosition;
+  export let bgRepeat;
+  export let bgOpacity;
   // Video Backgrounds
-  export let bgVideo = "";
-  export let bgVideoLoop = "";
-  export let bgVideoMuted = "";
+  export let bgVideo;
+  export let bgVideoLoop;
+  export let bgVideoMuted;
   // Iframe Backgrounds
-  export let bgIframe = "";
-  export let bgInteractive = "";
+  export let bgIframe;
+  export let bgInteractive;
 </script>
 
 <section
