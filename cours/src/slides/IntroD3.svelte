cdb9ec97c5310d48837c8f9c96dc334f633e61e1
romanoe <noemi.romano@protonmail.com>
2024-02-09 17:04:54 +0100

    refactor intro d3

--- cours/src/slides/IntroD3.svelte
+++ cours/src/slides/IntroD3.svelte
@@ -7,6 +7,7 @@
   import { onMount } from "svelte";
   import { transition } from "d3-transition";
   import { easeLinear } from "d3-ease";
+  import Presentation from "../Presentation.svelte";
 
   const width = 500;
   const height = 400;
@@ -97,7 +98,7 @@
             .attr("fill", "green")
             .text((d) => d)
         )
-        .attr("x", (d, i) => i * 30); // Mettre à jour à chaque fois la position
+        .attr("x", (d, i) => i * 40); // Mettre à jour à chaque fois la position
     }, 1000);
 
     setInterval(() => {
@@ -109,7 +110,7 @@
             enter
               .append("text")
               .attr("fill", "green")
-              .attr("x", (d, i) => i * 30) // Mettre à jour à chaque fois la position
+              .attr("x", (d, i) => i * 40) // Mettre à jour à chaque fois la position
               .text((d) => d),
           (update) => update.attr("fill", "grey").attr("x", (d, i) => i * 30) // Mettre à jour à chaque fois la position
         );
@@ -128,14 +129,14 @@
             enter
               .append("text")
               .attr("fill", "green")
-              .attr("x", (d, i) => i * 30)
+              .attr("x", (d, i) => i * 40)
               .attr("y", 0)
               .text((d) => d),
           (update) =>
             update
               .attr("fill", "grey")
               .attr("y", 0)
-              .attr("x", (d, i) => i * 30),
+              .attr("x", (d, i) => i * 40),
           (exit) =>
             exit
               .attr("fill", "brown")
@@ -151,7 +152,7 @@
 <Slide>
   <h3>Semaine passée</h3>
   <p class="fragment">
-    <span class="red">Fonction</span> Explicative ou explorsatoire <br />
+    <span class="red">Fonction</span> Explicative ou exploratoire <br />
   </p>
   <p class="fragment">
     <span class="red">Formes</span> Statique ou interactive <br />
@@ -198,7 +199,11 @@
   <br />
 
   <div class="center">
-    <img src="assets/02-intro-d3/doc_annotation.svg" alt="js-doc" width="60%" />
+    <img
+      src="assets/02-intro-d3//doc_annotation.svg"
+      alt="js-doc"
+      width="60%"
+    />
   </div>
   <br />
 
@@ -219,15 +224,15 @@
           méthodes
         </li>
       </ul>
-      <code>
-        <span class="red">objet</span>.methode1() <br />
-        .methode2() <br />
-        .methode3()
+      <code class="small">
+        objet.<span class="red">methode1()</span>.<span class="red"
+          >methode2()</span
+        >.<span class="red">methode3()</span>
       </code>
     </div>
     <div class="col-50">
       <img
-        src="assets/02-intro-d3/code_chaining_methods.png"
+        src="assets/02-intro-d3//code_chaining_methods.png"
         alt="chaining-methods"
       />
 
@@ -250,14 +255,24 @@
   <div class="row center">
     <div class="col-50">
       <h3>Sélectionner</h3>
-      <p>
-        Sélectionner des éléments du DOM pour ensuite les manipuler et modifier
-      </p>
-      <code class="small">d3.<b class="red">select</b>(<em>selector</em>)</code>
+      <code class="small">d3.<b class="red">select</b>(selector)</code>
       <br />
-      <code class="small"
-        >d3.<b class="red">selectAll</b>(<em>selector</em>)</code
-      >
+      <code class="small">d3.<b class="red">selectAll</b>(selector)</code>
+
+      <ul>
+        <li class="small">
+          <code>d3.<span class="red">select</span></code> : méthode qui permet de
+          sélectionner un élément unique du DOM en fonction d'un sélecteur CSS
+        </li>
+        <li class="small">
+          <code>d3.<span class="red">selectAll</span></code> : méthode qui permet
+          de sélectionner tous les éléments correspondant au sélecteur CSS donné
+        </li>
+        <li class="small">
+          <code class="red">selector</code> : le sélecteur CSS utilisé pour sélectionner
+          les éléments du DOM
+        </li>
+      </ul>
     </div>
     <div class="col-50">
       <small>
@@ -302,21 +317,32 @@
   <div class="row center">
     <div class="col-50">
       <h3>Modifier</h3>
-      <p>Modifier les attributs des éléments sélectionnés</p>
       <code class="small">selection.<b class="red">attr</b>(name[, value])</code
       >
+      <ul>
+        <li class="small">
+          <code class="red">attr</code> : méthode qui permet de modifier les attributs
+          des éléments sélectionnés
+        </li>
+        <li class="small">
+          <code class="red">name</code> : le nom de l'attribut à modifier
+        </li>
+        <li class="small">
+          <code class="red">value </code> (optionnel) : la valeur à assigner à l'attribut
+        </li>
+      </ul>
     </div>
     <div class="col-50">
       <div class="row center">
         <div class="col-50">
           <img
-            src="assets/02-intro-d3/code_modifier_html.png"
+            src="assets/02-intro-d3//code_modifier_html.png"
             alt="code-modifier-html"
           />
         </div>
         <div class="col-50">
           <img
-            src="assets/02-intro-d3/code_modifier_js.png"
+            src="assets/02-intro-d3//code_modifier_js.png"
             alt="code-modifier-js"
           />
         </div>
@@ -343,8 +369,16 @@
   <div class="row center">
     <div class="col-50">
       <h3>Créer</h3>
-      <p>Ajouter de nouveaux éléments DOM à la sélection existante</p>
       <code class="small">selection.<b class="red">append</b>(element)</code>
+      <ul>
+        <li class="small">
+          <code class="red">append</code> : méthode qui permet d'ajouter de nouveaux
+          éléments DOM à une sélection existante
+        </li>
+        <li class="small">
+          <code class="red">element</code> : le type d'élément à créer (balise HTML)
+        </li>
+      </ul>
     </div>
     <div class="col-50">
       <img src="assets/02-intro-d3/code_creer_js.png" alt="code-creer-js" />
@@ -368,25 +402,134 @@
   <div class="row center">
     <div class="col-50">
       <h3>Événements</h3>
-      <p>Attache un gestionnaire d'événements à la sélection</p>
       <code class="small"
         >selection.<b class="red">on</b>(eventType, callback)</code
       >
+      <ul>
+        <li class="small">
+          <code class="red">on</code> : méthode qui permet d'attacher des gestionnaires
+          d'événements à une sélection d'éléments du DOM
+        </li>
+        <li class="small">
+          <code class="red">eventType</code> : le type d'événement que vous
+          souhaitez gérer (par exemple, <em>click</em>, <em>mouseover</em>,
+          <em>keydown</em>, etc.).
+        </li>
+        <li class="small">
+          <code class="red">callback</code> : la fonction de rappel (callback) qui
+          sera exécutée lorsque l'événement se produit.
+        </li>
+      </ul>
     </div>
     <div class="col-50">
-      <img src="assets/02-intro-d3/code_events.png" alt="code-events" />
-      <p>x: {x}, y: {y}</p>
-      <svg id="mon-svg3"> </svg>
+      <img src="assets/02-intro-d3//code_events.png" alt="code-events" />
+      <!-- <p>x: {x}, y: {y}</p>
+      <svg id="mon-svg3"> </svg> -->
     </div>
   </div>
-
-  <br />
 
   <small
     ><a href="https://github.com/d3/d3-selection/tree/v3.0.0#handling-events"
       ><p><code class="red"> ↳ d3-selection - Handling events</code></p></a
     ></small
   >
+</Slide>
+
+<Slide>
+  <div class="row center">
+    <div class="col-50">
+      <h3>Données</h3>
+      <code class="small"
+        >selection.<b class="red">data</b>(tableauDonnees[,key])</code
+      >
+      <ul>
+        <li class="small">
+          <code class="red">data</code> : méthode qui permet de lier les données
+          aux éléments du DOM existants.
+        </li>
+        <li class="small">
+          <code class="red">tableauDonnees</code> : tableau de données à lier aux
+          éléments du DOM.
+        </li>
+        <li class="small">
+          <code class="red">key</code> (optionnel): fonction ou une valeur qui spécifie
+          comment faire correspondre les données aux éléments du DOM existants en
+          utilisant une clé. Si la clé n'est pas fournie, la correspondance est faite
+          par indice.
+        </li>
+      </ul>
+    </div>
+    <div class="col-50">
+      <img
+        src="assets/02-intro-d3/data-enter-update-exit.svg"
+        alt="join-data-drawing"
+        width="700"
+      />
+    </div>
+  </div>
+  <small
+    ><a href="https://d3js.org/d3-selection/joining"
+      ><p>
+        <code class="red"> ↳ d3-selection | Joining data</code>
+      </p></a
+    ></small
+  ></Slide
+>
+
+<Slide>
+  <div class="row center">
+    <div class="col-50">
+      <h3>Données</h3>
+      <code class="small"
+        >selection.<b class="red">data</b>(data).<b class="red">join</b>(enter,
+        update, exit)</code
+      >
+      <ul>
+        <li class="small">
+          <code class="red">join</code> : méthode introduite dans D3.js version 6,
+          simplifiant la gestion des nouvelles données, des mises à jour et des éléments
+          à supprimer
+        </li>
+        <li class="small">
+          <code class="red">enter</code> : représente les nouveaux éléments à créer
+          pour correspondre aux nouvelles données.
+        </li>
+        <li class="small">
+          <code class="red">update</code> : représente les éléments existants qui
+          correspondent aux données actuelles et qui doivent être mis à jour.
+        </li>
+
+        <li class="small">
+          <code class="red">exit</code> : représente les éléments sans correspondance
+          avec les données actuelles et qui doivent être supprimés.
+        </li>
+      </ul>
+    </div>
+    <div class="col-50">
+      <img
+        src="assets/02-intro-d3/data-enter-update-exit.svg"
+        alt="join-data-drawing"
+        width="600"
+      />
+      <img
+        src="assets/02-intro-d3/data-enter-update-exit-letters.svg"
+        alt="enter-update-exit-letters"
+        width="600"
+      />
+    </div>
+  </div>
+  <small
+    ><a href="https://d3js.org/d3-selection/joining"
+      ><p>
+        <code class="red"> ↳ d3-selection | Joining data</code>
+      </p></a
+    ></small
+  ></Slide
+>
+
+<Slide>
+  <h3>Joindre les données</h3>
+  <h5><span class="heig-red">EXEMPLE</span></h5>
 </Slide>
 
 <Slide>
@@ -400,15 +543,15 @@
         <li>Créer un dossier github pour le projet</li>
         <li>
           Suivre directives <a
-            href="https://github.com/romanoe/visualdon-exercices/tree/main/projet"
-            >ici</a
+            href="https://github.com/romanoe/comem-visualdon/projet">ici</a
           >
         </li>
       </ul>
     </div>
 
     <div class="col-40">
-      <img src="assets/02-intro-d3/data.gif" alt="data-gif" /> <br />
+      <img src="assets/02-intro-d3//data.gif" alt="data-gif" />
+      <br />
     </div>
   </div>
 </Slide>
@@ -438,7 +581,7 @@
     <span class="red">Rajouter dossier du cours en upstream</span> <br />
     <small
       ><code
-        >git remote add upstream git@github.com:romanoe/visualdon-exercices.git</code
+        >git remote add upstream git@github.com:romanoe/comem-visualdon.git</code
       ></small
     >
   </p>
